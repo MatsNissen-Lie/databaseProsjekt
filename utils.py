3c1b616f89ce64590251cdd5c69db3f366a5d87f
Mats Alexander Nissen-Lie <matsani@stud.ntnu.no>
2024-03-12 13:24:15 +0100

    change structure

---
+++ utils.py
@@ -0,0 +1,12 @@
+import os
+
+
+def get_file_name_of_newest_database():
+    base_name = "database"
+    counter = 1
+
+    # Increment counter until a new file name is found
+    while os.path.exists(f"{base_name}{counter}.db"):
+        counter += 1
+    counter -= 1
+    return f"{base_name}{counter}.db"
