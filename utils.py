38915bff8aeb1a30154667a6d8e31f6d8c573dc0
Mats Alexander Nissen-Lie <matsani@stud.ntnu.no>
2024-03-12 13:24:56 +0100

    change utils

--- utils.py
+++ utils.py
@@ -10,3 +10,14 @@ def get_file_name_of_newest_database():
         counter += 1
     counter -= 1
     return f"{base_name}{counter}.db"
+
+
+def generate_new_database_filename():
+    base_name = "database"
+    counter = 1
+
+    # Increment counter until a new file name is found
+    while os.path.exists(f"{base_name}{counter}.db"):
+        counter += 1
+
+    return f"{base_name}{counter}.db"
